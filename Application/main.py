f3f1968ee70f35731c814795bae52cdb2741d23c
Kazuma Akutsu <kaukutsu99@outlook.jp>
2026-04-18 16:07:27 +0900

    http_six

--- Application/main.py
+++ Application/main.py
@@ -9,5 +9,5 @@ handler = http.server.SimpleHTTPRequestHandler
 httpd = http.server.HTTPServer(server_address, handler)
 
 #サーバーを起動
-print("HTTP Server Running on http://127.0.0.1:8000")
+print("HTTP Server Running on localhost:8000")
 httpd.serve_forever()
